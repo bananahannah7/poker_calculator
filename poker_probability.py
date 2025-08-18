886ef5aebe86c4815eab6cdbfbbf1b6d76f5f406
Hannah Rosenberg <emirodriguez2025@gmail.com>
2025-08-18 00:03:54 -0700

    notes on next steps for code

--- poker_probability.py
+++ poker_probability.py
@@ -1,16 +1,16 @@
 import random
 from collections import Counter
 
+"""
+This project takes in a series of cards provided and calculates your chance of having the best hand of the players. 
+It depends on what cards you have, what cards are down, and how many people are playing (and how many cards still need to be put down).
+
+It works specifically for texas hold 'em poker where you get two cards in your hand and three cards are placed down, then one more, 
+then one last card, and you make the best hand of five using the total possible seven cards. 
+"""
 SUITS = ["H", "D", "C", "S"] # hearts, diamonds, clubs, and spades
 RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"] # ace marked as highest for high card reasons
 
-
-"""
-
-
-
-
-"""
 
 class Card: 
     def __init__(self, rank, suit):
@@ -69,23 +69,34 @@ def hand_evaluator(cards):
 
     # need one case for A-5 straight since A is at bottom not top
     # need one case for all other straights
+    # might be worth making case for royal straight here? 
 
     # royal flush is straight and flush and A K Q J 10
+    # use flush and straight and check for the five cards
+
     # straight flush is straight and flush
+    # use flush and straight
 
     # four of a kind
+    # check for sorted list, four of ranks are same
 
     # full house (three of a kind and pair)
+    # check for sorted list, three of ranks are same, two of ranks are same
+
 
     # flush
+    # use the flush marker
 
     # straight
 
     # three of a kind
+    # check for sorted list, three of ranks are same
     
     # two pair
+    # check for sorted list, two of ranks are same for two different ranks
 
     # one pair
+    # check for sorted list, two of ranks are same
 
 def compare_hands(hand1, hand2):
     score1 = hand_evaluator(hand1)
